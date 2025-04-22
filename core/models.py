3b9be5761214d9008b1cb502dce981ebb4755b0a
jaysonar092 <jaysonar092@gmail.com>
2025-04-23 00:15:22 +0530

    upload profile picture

--- core/models.py
+++ core/models.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.models import User
 from django.db import models
 
+def user_profile_picture_path(instance, filename):
+    # upload path will be: media/profile_pictures/user_<id>/<filename>
+    return f'profile_pictures/user_{instance.user.id}/{filename}'
+
 class Room(models.Model):
     number = models.CharField(max_length=10, unique=True)
     capacity = models.IntegerField()
@@ -25,11 +29,11 @@ class RoomRequest(models.Model):
     def __str__(self):
         return f"Request by {self.student or self.teacher} - {self.status}"
 
-
 class StudentProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     roll_number = models.CharField(max_length=20, unique=True)
     room = models.ForeignKey('Room', on_delete=models.SET_NULL, null=True, blank=True)
+    profile_picture = models.ImageField(upload_to=user_profile_picture_path, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
@@ -38,6 +42,7 @@ class TeacherProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     employee_id = models.CharField(max_length=20, unique=True)
     room = models.ForeignKey('Room', on_delete=models.SET_NULL, null=True, blank=True)
+    profile_picture = models.ImageField(upload_to=user_profile_picture_path, null=True, blank=True)
 
     def __str__(self):
         return self.user.username
